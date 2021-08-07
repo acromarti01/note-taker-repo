88db653e09e309a978f925370cca2063e73b54dd
Anthony Cromartie <acromarti@gmail.com>
2021-08-06 21:36:22 -0500

    refactor changes in notes.js

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -118,7 +118,9 @@ const handleRenderSaveBtn = () => {
 
 // Render the list of note titles
 const renderNoteList = async (notes) => {
+  //console.log("THIS IS NOTES", notes);
   let jsonNotes = await notes.json();
+  //console.log("THIS IS JSON NOTES", typeof(jsonNotes), jsonNotes);
   noteList = document.querySelectorAll('.list-container .list-group');
   if (window.location.pathname === '/notes') {
     noteList.forEach((el) => (el.innerHTML = ''));
--- routes/notes.js
+++ routes/notes.js
@@ -2,20 +2,24 @@ const notes = require('express').Router();
 const { readFromFile, readAndAppend, writeToFile } = require("../helpers/fsUtils");
 const { v4: uuidv4 } = require('uuid');
 
-notes.get("/notes", async(request, response) =>{
-    const data = await readFromFile("./db/db.json", "utf8");
-    response.json(JSON.parse(data));
+// notes.get("/notes", async(request, response) =>{
+//     const data = readFromFile("./db/db.json", "utf8");
+//     response.json(JSON.parse(data));      
+// });
+notes.get("/notes", (request, response) =>{
+    readFromFile("./db/db.json", "utf8").then(data => response.json(JSON.parse(data))); 
+    /*The readFromFile is a promise so needs a ".then".  The reason data needs to be parsed is because jsonNotes
+    is a string and needs to be a object*/
 });
 
-notes.post("/notes", async(request, response) =>{
+notes.post("/notes", (request, response) =>{
     const note = {
         title: request.body.title,
         text: request.body.text,
         id: uuidv4()
     };
-    readAndAppend(note, './db/db.json');
-    const data = await readFromFile("./db/db.json", "utf8");
-    response.json(JSON.parse(data));
+    readAndAppend(note, './db/db.json');    
+    response.json(note);    
 });
 
 notes.delete("/notes/:id", async(request, response) => {
@@ -23,7 +27,6 @@ notes.delete("/notes/:id", async(request, response) => {
     const data = await readFromFile("./db/db.json", "utf8");
     const jsonData = JSON.parse(data);
     const filteredJsonData = jsonData.filter((note) => note.id !== id);
-
     writeToFile("./db/db.json", filteredJsonData);
 
     response.json(`Note "${id}" has been deleted`);
